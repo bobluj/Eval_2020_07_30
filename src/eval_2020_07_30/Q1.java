1fc9d487e4230c827e7827c5f8875479b37b3bc0
JULES RACHET <rachet.jules@gmail.com>
2020-07-30 10:46:50 +0200

    EXAM FILLED

--- src/eval_2020_07_30/Q1.java
+++ src/eval_2020_07_30/Q1.java
@@ -5,17 +5,84 @@
  */
 package eval_2020_07_30;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jules.rachet
  */
 public class Q1 {
 
+
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        int[] Tab = {12,12,11,10,9};
+        int[][] Tab2;
+        int Occur1;
+        int Occur2;
+        int i;
+        int j;
+
+        Occur1 = NbOccur(Tab,4 );
+        Tab2 = FetchTab(Tab, 4);
+        Occur2 = NbOccur(Tab2, 4);
+
+        System.out.println(Occur1);
+        System.out.println(Occur2);
+    }
+        public static int NbOccur(int[] Tab, int n)
+    {
+        int i;
+        int count;
+
+        count = 0;
+
+        for(i = 0; i< Tab.length; i++)
+        {
+             if (n == Tab[i])
+                count++;
+        }
+        return(count);
     }
 
+       public static int NbOccur(int[][] Tab, int n)
+    {
+        int i;
+        int j;
+        int occur;
+
+        for (i = 0; i < Tab.length; i++)
+        {
+            for (j = 0; j < 2; j++)
+            {
+               if (Tab[i][0] == n)
+               {
+                   occur = Tab[i][1];
+                    return(occur);
+               }
+            }
+        }
+        return 0;
+    }
+
+        public static int[][] FetchTab(int[] Tab,int n)
+    {
+        int i;
+        int j;
+        int[][] TabConcat = new int[Tab.length][Tab.length];
+
+        for (i = 0; i < Tab.length; i++)
+        {
+             TabConcat[i][0] = Tab[i];
+        }
+        for (i = 0; i < Tab.length; i++)
+        {
+            TabConcat[i][1] = NbOccur(Tab, n);
+        }
+                   return(TabConcat);
+    }
 }
